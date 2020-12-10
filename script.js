96283b173fcd161cd196b08a3e479568e00cf917
deepyes02 <deepyes@outlook.com>
2020-12-10 21:04:02 +0545

    changed landing image

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //this is working ;-)
+setTimeout(function(){
+    document.getElementById("cta-title").style.top = "0px";
+}, 200)
+
+setTimeout(function(){
+    document.getElementById("cta-subtitle").style.left = "0px";
+}, 200)
